81d50419afb475568124a1d2b3ef2d1838ff26c1
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-01-26 13:51:58 +0300

    Add files via upload

---
+++ string/string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+
+
+ int main(){
+    char ch1[5]="hi";
+    char ch2[5]="hello";
+    char ch3[5];
+    printf("%s\n",strrev(ch2));
+    printf("%s\n",strupr(ch2));
+    strcpy(ch3,ch2);
+    printf("%s",ch3);
+
+    getch();
+
+ }
