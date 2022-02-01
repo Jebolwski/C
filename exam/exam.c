1ec8765a16fdf1cea7b4deab6441ad0f1eca2ee7
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-02-01 18:17:52 +0300

    Add files via upload

---
+++ exam/exam.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+// typedef enum{
+//     bay,
+//     bayan,
+// }cinsiyet;
+
+// typedef struct
+// {
+//     int idno;
+//     char *isim;
+//     cinsiyet c;
+//     int dogum_yili;
+//     char *okul;
+//     float ortalama;
+
+// }ogr;
+
+// void AsalIdListele(ogr){
+//     for(int i=0;i<50;i++){
+//         if(ogr.id%2==1){
+//             return ogr.okul;
+//         }
+//     }
+// }
+
+// int EnKucukErkek(){
+//     int erkekdizi[50];
+//     for(int i=0;i<50;i++){
+//         if(ogr.c==bay)
+//         erkekdizi.push(2022-ogr.dogumyili);
+//     }
+// }
+
+
+// float SayiUret(){
+
+//     int rasgelesayi=rand
+// }
+
+int main(){
+
+    // ogr ahmet;
+    // ahmet.idno=57;
+    // ahmet.isim="Ahmet Berber";
+    // ahmet.c=bay;
+    // ahmet.okul="Sakarya Üniversitesi";
+    // ahmet.ortalama=3.08;
+
+
+    float x=22/5;
+    printf("%f", x);
+
+
+
+
+}
