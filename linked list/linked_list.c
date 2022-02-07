396d21199d1e1300794c82659284b8dc2642f597
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-02-07 13:05:30 +0300

    Add files via upload

---
+++ linked list/linked_list.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+
+typedef struct{
+    int x;
+    n *next;
+}n;
+
+typedef n node;
+
+int main(){
+    node *root;
+    root = (node *)malloc(sizeof(node));
+    root->x=1;
+    root->next = (node *)malloc(sizeof(node));
+    root->next->x = 2;
+    root->next->next = (node *)malloc(sizeof(node));
+    root->next->next->x = 3;
+    root->next->next->next = NULL;
+    node *iter;
+    iter = root;
+    printf("%d",iter->x);
+    iter=iter->next;
+    printf("%d",iter->x);
+}
