30e0ea895fb2de393f03c75d1767f6f88aa8fddd
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-02-18 10:52:32 +0300

    Add files via upload

--- binary trees/binary_trees.c
+++ binary trees/binary_trees.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <conio.h>
-
+#include <stdbool.h>
 
 typedef struct n n;
 
@@ -17,51 +17,53 @@ node *root;
 node *iter;
 node *temp;
 
-node * ekle(node *root,int a){
+bool ekle(node *root,int a){
     if(root==NULL){
         root = (node *)malloc(sizeof(node));
         root->x=a;
         root->sag=NULL;
         root->sol=NULL;
+        return false;
     }
     else{
         if(a>root->x){
             if(root->sag==NULL){
                 root->sag=(node *)malloc(sizeof(node));
                 root->sag->x=a;
-                return root;
             }
             else{
             ekle(root->sag,a);
-            return root;
             }
         }
-        else if(a<root->x){
+        if(a<root->x){
             if(root->sol==NULL){
                 root->sol=(node *)malloc(sizeof(node));
                 root->sol->x=a;
-                return root;
             }
             else{
-                ekle(root->sol,a);
-                return root;
+                ekle(root->sol , a);
             }
         }
     }
 }
 
-void dolas(node *root){
+bool dolas(node *root){
     if (root==NULL){
-        return;
+        return false;
     }
     dolas(root->sol);
     printf("%d ",root->x);
     dolas(root->sag);
+    return true;
 }
 
 
 int main(){
-    node *root = (node *)malloc(sizeof(node));
-    root->sag=(node *)malloc(sizeof(node));
+
+    ekle(root,10);
+    ekle(root,20);
+    ekle(root,30);
+    ekle(root,40);
+    dolas(root);
 
 }
