4d30515bf6dd24318a566daf0e3c838ce999291d
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-02-27 12:51:41 +0300

    Add files via upload

--- string/string_2.c
+++ string/string_2.c
@@ -11,55 +11,56 @@ int uzunluk(char *p){
     return uzunluk;
 }
 
+char *bastir(char *p){
+    printf("%s\n",p);
+}
+
 char *tersecevir(char *p){
-    char *new_p;
-    for(int i=0;i<uzunluk(p);i++){
-        new_p[i]=p[uzunluk(p)-i-1];
-    }
-    printf("%s\n",new_p);
-    return new_p;
+    char temp[uzunluk(p)];
 
+    for(int i=0;i<uzunluk(p)-1;i++){
+        temp[i]=p[uzunluk(p)-i];
+    }
+
+    char *temp_p;
+    temp_p=&temp;
+    
+    return temp_p;
 }
 
-int kopyalama(char *p,char *p1){
+char *kopyalama(char *p,char *p1){
     int i=0;
-    for(;p[i]!='\0';i++){
-        p1[i]=p[i];
-    }
-    int j=uzunluk(p);
-    for(;p1[j]!='\0';j++){
-        p[j]=0;
-    }
-    return 0;
+    p=p1;
 }
 
-
-int esitmi(char *p,char *p1){
+char *esitmi(char *p,char *p1){
     int i=0;
     int count=0;
-    for(;p[i]!='\0';i++){
-        if(p[i]==p1[i] && uzunluk(p)==uzunluk(p1)){
-            count++;
-        }
-        if(count==uzunluk(p) || count==uzunluk(p1)){
-            return "Eşit";
-        }
+    if(p==p1){
+        return "Eşit";
+    }
+    else{
         return "Eşit Değil";
     }
 }
+
+char *sonelemansilme(char *p){
+    int soneleman = uzunluk(p)-1;
+    char temp=p;
+    p[soneleman] = NULL; 
+    return "Önceki Hali :",temp,"Sonraki Hali :",p;  
+}
+
 int main(){
     char text[]="Ali";
-    char text1[]="Ali";
+    char text1[]="Aliasdad";
     char *p;
+    p=&text;
     char *p1;
     p1=&text1;
-    p=&text;
-    printf("%s\n",p);
-    printf("%i\n",uzunluk(p));
-    printf("%s\n",esitmi(p1,p));
-    kopyalama(p,p1);
-    printf("%s\n",p1);
-    tersecevir(p);
-
-    
+    // bastir(p);
+    // printf("Uzunluk : %i\n",uzunluk(p));
+    // printf("Girdiler %s\n",esitmi(p1,p));
+    // kopyalama(p,p1);
+    printf("%s\n",tersecevir(p1));
 }
