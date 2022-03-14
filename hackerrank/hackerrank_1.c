e4ead4e1d2b99be8b18bc4af37e5900b61fb4ca6
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-03-14 11:58:25 +0300

    four more

--- hackerrank/hackerrank_1.c
+++ hackerrank/hackerrank_1.c
@@ -58,3 +58,136 @@ int main()
     
     return 0;
 }
+
+// https://www.hackerrank.com/challenges/functions-in-c
+
+int max_of_four(int a, int b, int c, int d){
+    if(d>a && d>b && d>c){
+        return d;
+    }
+    else if (b>a && b>d && b>c) {
+        return b;
+    }
+    else if (c>a && c>b && c>d) {
+        return c;
+    }
+    else{
+        return a;
+    }
+    
+    
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
+// https://www.hackerrank.com/challenges/pointer-in-c
+
+
+void update(int *a,int *b) {
+    printf("%d\n%d\n",*a+*b,abs(*b-*a));
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int *pa = &a, *pb = &b;
+    update(pa, pb);
+
+    return 0;
+}
+
+// https://www.hackerrank.com/challenges/conditional-statements-in-c
+
+int main()
+{
+    char* n_endptr;
+    char* n_str = readline();
+    int n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    if(n==1){
+        printf("one");
+    }
+    else if (n==2) {
+        printf("two");
+    }
+    else if (n==3) {
+        printf("three");
+    }
+    else if (n==4) {
+        printf("four");
+    }
+    else if (n==5) {
+        printf("five");
+    }
+    else if (n==6) {
+        printf("six");
+    }
+    else if (n==7) {
+        printf("seven");
+    }
+    else if (n==8) {
+        printf("eight");
+    }
+    else if (n==9) {
+        printf("nine");
+    }
+    else{
+        printf("Greater than 9");
+    }
+    return 0;
+}
+
+// https://www.hackerrank.com/challenges/for-loop-in-c/
+
+int main() 
+{
+    int a, b;
+    scanf("%d\n%d", &a, &b);
+  	for(int i = a;i<=b;i++){
+          if(i==1){
+        printf("one\n");
+    }
+    else if (i==2) {
+        printf("two\n");
+    }
+    else if (i==3) {
+        printf("three\n");
+    }
+    else if (i==4) {
+        printf("four\n");
+    }
+    else if (i==5) {
+        printf("five\n");
+    }
+    else if (i==6) {
+        printf("six\n");
+    }
+    else if (i==7) {
+        printf("seven\n");
+    }
+    else if (i==8) {
+        printf("eight\n");
+    }
+    else if (i==9) {
+        printf("nine\n");
+    }
+    else{
+        if(i%2==0){
+            printf("even\n");
+        }
+        else {
+            printf("odd\n");
+        }
+      }
+      }
+    return 0;
+}
