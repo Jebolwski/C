6314dc2d13422f49cb8b88a4cfb3a0110957b257
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-03-15 11:31:39 +0300

    two more

--- hackerrank/hackerrank_1.c
+++ hackerrank/hackerrank_1.c
@@ -227,3 +227,67 @@ int main()
     free(arr);
     return 0;
 }
+
+// https://www.hackerrank.com/challenges/recursion-in-c/
+
+int find_nth_term(int n, int a, int b, int c) {
+  if(n==1){
+      return a;
+  }
+  else if(n==2){
+      return b;
+  }
+  else if(n==3){
+      return c;
+  }
+  else{
+      return find_nth_term(n-1,a,b,c);
+  }
+}
+
+int main() {
+    int n, a, b, c;
+  
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+ 
+    printf("%d", ans); 
+    return 0;
+}
+
+// https://www.hackerrank.com/challenges/students-marks-sum/
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+    int sum=0;
+    if(gender == 'g'){
+        for(int i=1;i<number_of_students;i=i+2){
+          sum=sum+*(marks+i);
+        }
+    }
+    else{
+        for(int i=0;i<number_of_students;i=i+2){
+          sum=sum+*(marks+i);
+      }
+    }
+    return sum; 
+}
+
+int main() {
+    int number_of_students;
+    char gender;
+    int sum;
+  
+    scanf("%d", &number_of_students);
+    int *marks = (int *) malloc(number_of_students * sizeof (int));
+ 
+    for (int student = 0; student < number_of_students; student++) {
+        scanf("%d", (marks + student));
+    }
+    
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+ 
+    return 0;
+}
