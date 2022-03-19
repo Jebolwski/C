7bd413dfcf4e81639637efc177bdb604ecb989f6
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-03-19 11:55:14 +0300

    Add files via upload

---
+++ bitwise/bitwise1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+
+
+int main(){
+    int a = 10;
+    int b = 20;
+
+    printf("XOR : %d\n",a^b);
+    printf("AND : %d\n",a&b);
+    printf("OR : %d\n",a|b);
+    printf("NOT A : %d NOT B: %d\n",~a,~b);
+
+
+}
