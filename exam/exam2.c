3e220c10751a9f9d122986270609b89fac866c0b
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-03-21 16:43:01 +0300

    Add files via upload

---
+++ exam/exam2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+
+int main(){
+    int a;
+    int b;
+    for(a=0;a<10;a++){
+        printf("*");
+        for(b=0;b<a;b++){
+            printf("*");
+        } 
+            printf("\n");   
+    }
+    for(a=0;a<10;a++){
+        printf("*");
+        for(b=10;b>a;b--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("*");
+    for(a=0;a<10;a++){
+        printf(" ");
+        for(b=10;b>a;b--){
+            printf(" ");
+        }
+        for (int i = 0; i < 4*a; i=i+2){
+            printf("*");
+        }
+        for(int c=10;c>a;c--)
+        {
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
