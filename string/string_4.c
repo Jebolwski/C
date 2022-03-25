0d3ffcc9c0fb58c25e74fa056a4cb8f4204255f2
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-03-25 16:36:21 +0300

    Add files via upload

---
+++ string/string_4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+
+
+
+int main(){
+
+    char string[50];
+    printf("Enter your name : ");
+    scanf("%s",string);
+    printf("Your old name : %s\n",string);
+    string[0]="F";
+    // int a=0;
+    // while(a<strlen(string)){
+    //     a=a+1;
+    //     if(str[a]== 'a' || str[a] == 'A' || str[a] == 'e' || str[a] == 'E' || str[a] == 'i' || str[a] == 'I' || str[a] =='o' || str[a]=='O' || str[a] == 'u' || str[a] == 'U'){
+    //         str[a+2]=str[a+1];
+    //         str[a+1] = '-';
+    //         for(int i=a+2;i<strlen(string);i=i+2){
+    //             str[i+1]=str[i];
+    //         }
+    //     }
+    // }
+    printf("Your new name : %s",string);
+    getch();
+}
