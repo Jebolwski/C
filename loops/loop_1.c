6fe29d42017cea99bd98651c84176bf42a5ed86d
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-03-29 10:41:34 +0300

    Add files via upload

--- loops/loop_1.c
+++ loops/loop_1.c
@@ -32,19 +32,61 @@ int main(){
     // {
     //     printf("%d * %d : %d \n",a2,i,a2*i);
     // }
-    int i;
-    for(i=1;i<=10;i++){
-        printf("%d",i);
-        for(int j=1;j<i;j++){
-            printf("%d",j);
+    // int i;
+    // for(i=1;i<=10;i++){
+    //     printf("%d",i);
+    //     for(int j=1;j<i;j++){
+    //         printf("%d",j);
+    //     }
+    //     for(int a=10;a>=i;a--){
+    //         printf(" ");
+    //     }
+    //     for(int j=1;j<i;j++){
+    //         printf("%d",j);
+    //     }
+    //     printf("\n");
+    // }
+
+    // int count=0;
+
+    //     for(int i=1;i<=10;i++){
+    //         count=1;
+    //         printf("%d",count);
+    //         for(int j=1;j<i;j++){
+    //             count=0;
+    //             printf("%d",count);
+    //             count=1;
+    //         }
+    //         printf("\n");
+    //     }
+    int input=13;
+    int count=0;
+    // printf("Enter a number : ");
+    // scanf("%d",&input);
+    for(int i =1;i<input;i++){
+        if(i%2!=0 && i%3!=0 && i%3!=0 && i%5!=0 && i%7!=0 && i<input){
+            count=count+1;
         }
-        for(int a=10;a>=i;a--){
-            printf(" ");
-        }
-        for(int j=1;j<i;j++){
-            printf("%d",j);
-        }
-        printf("\n");
     }
+    int dizi[count];
+    dizi[0]=2;
+    dizi[1]=3;
+    dizi[2]=5;
+    dizi[3]=7;
+    int co=4;
+    for(int i =2;i<input;i++){
+        if(i%2!=0 && i%3!=0 && i%3!=0 && i%5!=0 && i%7!=0 && i<input){
+            dizi[co]=i;
+            co=co+1;
+        }
+    }
+    for(int i=1;i<count;i++){
+        for(int j=i+1;i<count;j++){
+            if (dizi[i]+dizi[j]==input){
+                printf("%d + %d = %d",i,j,i+j);
+            }
+        }
+    }
+    
 }
 
