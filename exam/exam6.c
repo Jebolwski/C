d7880cc6c72fcd75f01e9b61a859f1dd5dc77b32
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-04-05 18:34:12 +0300

    Add files via upload

--- exam/exam6.c
+++ exam/exam6.c
@@ -2,12 +2,23 @@
 
 int main(){
     int i=0;
+
+    int sayfa=0,gun=0,u=10;
+    while(sayfa<2800){
+        sayfa=sayfa+u;
+        u=u+5;
+        gun++;
+    }
+    printf("Okunan gun sayisi : %d\n",gun);
     while(1){
         i++;
-        if(i%2==0) printf("*");
+        if(i%2==1) printf("*");
         else printf("+");
+        if(i==100){
+            break;
+        }
     }
-
+    printf("\n");
     for(int i=-4;i<6;i++){
         printf("%d ",i);
     }
