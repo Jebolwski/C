d57ff7a4c31086b1f3fddbff7dd4643a4ef710f5
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-04-11 16:10:58 +0300

    Add files via upload

--- function/function_3.c
+++ function/function_3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <conio.h>
+#include <string.h>
 
 
 
@@ -44,9 +45,64 @@ int ekok(int num1,int num2){
 
 }
 
+int binary(int num){
+    int array[8];
+    int i = 0;
+    printf("%d in binary : ",num);
+    while(i<8){
+        array[i] = num%2;
+        num=num/2;
+        i++;
+    }
+    for (int i = 8; i > 0; i--)
+    {   
+        if(i==4){
+            printf(" ");
+        }
+        printf("%d",array[i]);
+    }
+    
+}
+
+int anagram(char str1[],char str2[]){
+    if(strlen(str1)!=strlen(str2)){
+        printf("Anagram degil.");
+    }
+    else{
+        int d=0;
+        for (int i = 0; i < strlen(str1); i++)
+        {
+            for (int j = 0; j < strlen(str2); j++)
+            {
+                if(str1[i]==str2[j]){
+                    printf("%c==%c\n",str1[i],str2[i]);
+                }
+                d++;
+            }
+        }
+        if(d==strlen(str1)^2){
+            printf("Anagramlar.");
+        }
+        else{
+            printf("Wat %d!=%d",d,strlen(str1));
+        }
+    }
+
+
+}
+
 int main(){
 
     ebob(21,2);
     ekok(22,4);
-
+    printf("\n");
+    binary(256);
+    char str1[40];
+    char str2[40];
+    printf("\nBirinci kelime : ");
+    scanf("%s",str1);
+    printf("Ikinci kelime : ");
+    scanf("%s",str2);
+    anagram(str1,str2);
+    getch();
 }
