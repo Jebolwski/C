f78803117541f50404080865977e07f28a2e18fc
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-04-18 09:43:03 +0300

    ortalama ve 3 ün üssü

--- exercise/exercise6.c
+++ exercise/exercise6.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <conio.h>
 
 
@@ -6,7 +7,6 @@ int main(){
     int num;
     printf("Enter triangles height : ");
     scanf("%d",&num);
-    printf("\n");
     for (int i = 0; i < num; i++)
     {
         printf("*");
@@ -22,6 +22,35 @@ int main(){
         }
         printf("\n");
     }
+    int num1;
+    printf("Enter a num : ");
+    scanf("%d",&num1);
+    for (int i = 1; i < 120; i++)
+    {
+        int a = pow(3,i);
+        if(num1==a){
+            printf("Bu sayi 3 un %d. ussu.",i);
+            break;
+        };
+        if(i==119){
+            printf("Bu sayi 3 un ussu degil.");
+            break;
+        }
+    }
+
+    int num2;
+    printf("\nEnter a num : ");
+    scanf("%d",&num2);
+    int total=0;
+    int a = 10;
+    for (int i = 1; i <= num2; i++)
+    {
+        total=total+a;
+        a=a+10;
+        printf("%d ortalama : %f\n",i,(float) total/i);
+    }
+    
+    
     getch();
 
 
