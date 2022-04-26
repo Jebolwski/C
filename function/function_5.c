6cba692433ca8fc99b57bf3ceda8e27d65177957
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-04-26 11:15:57 +0300

    Add files via upload

--- function/function_5.c
+++ function/function_5.c
@@ -15,7 +15,78 @@ int kucuk_harfler(char kelime[]){
     {
         printf("%c",kelime[i]);
     }
+    getch();
+}
+
+void cesar_sifreleme(char kelime[]){
     
+    for (int i = 0; i < strlen(kelime); i++)
+    {
+        if (65<=kelime[i]<=90 || 97<=kelime[i]<=122){
+            if(kelime[i]==90){
+               kelime[i] = kelime[i] + 7; 
+            }
+            else if (kelime[i]==122)
+            {
+                kelime[i] = kelime[i] - 57;
+            }
+                kelime[i] = kelime[i] + 1;
+        }
+    }
+    printf("Ceasar sifreli hali : ");
+    for (int i = 0; i < strlen(kelime); i++)
+    {
+        printf("%c",kelime[i]);
+    }
+
+    getch();
+}
+
+void rot_sifreleme(char kelime[]){
+    
+    for (int i = 0; i < strlen(kelime); i++)
+    {
+        if (65<=kelime[i]<=90 || 97<=kelime[i]<=122){
+            if(65<=kelime[i]<=78){
+                kelime[i] = kelime[i] + 13; 
+            }
+            else if (78<=kelime[i]<=90)
+            {
+                kelime[i] = kelime[i] - 13;
+            }
+            else if (90<=kelime[i]<=103)
+            {
+                kelime[i] = kelime[i] + 13;
+            }
+            else{
+                kelime[i] = kelime[i] - 13; 
+            }
+        }
+    }
+    printf("\nRot sifreli hali : ");
+    for (int i = 0; i < strlen(kelime); i++)
+    {
+        printf("%c",kelime[i]);
+    }
+
+    getch();
+}
+
+void gercek_sifreleme(char kelime[]){
+    
+    for (int i = 0; i < strlen(kelime); i++)
+    {
+        if (65<=kelime[i]<=90 || 97<=kelime[i]<=122){
+            int random = (int) rand()%26;
+            kelime[i] = kelime[i]+random;
+        }
+    }
+    printf("\nGercek sifreli hali : ");
+    for (int i = 0; i < strlen(kelime); i++)
+    {
+        printf("%c",kelime[i]);
+    }
+
     getch();
 }
 
@@ -24,5 +95,7 @@ int main(){
     char kelime[100];
     printf("Bir cumle ya da kelime girin : ");
     fgets(kelime,100,stdin);
-    kucuk_harfler(kelime);
+    cesar_sifreleme(kelime);
+    rot_sifreleme(kelime);
+    gercek_sifreleme(kelime);
 }
