d10b1568abc8e4a431326268d73981885a5a985c
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-05-06 12:28:07 +0300

    Add files via upload

--- array/array4.c
+++ array/array4.c
@@ -1,57 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
-
-
-int main(){
+int main()
+{
     int uzunluk;
     printf("Dizinin uzunlugunu girin : ");
-    scanf("%d",&uzunluk);
+    scanf("%d", &uzunluk);
     int dizi[uzunluk];
-    for (int i = 0; i < uzunluk; i++)
-    {
-        printf("%d. eleman : ",i+1);
-        scanf("%d",&dizi[i]);
-    }
-    int yakinlik=abs(dizi[0]-dizi[1]);
-    for (int i = 0; i < uzunluk; i++)
-    {
-        for (int j = 0; j < uzunluk; j++)
-        {
-            if(i!=j && yakinlik>abs(dizi[i]-dizi[j])){
-                yakinlik = abs(dizi[i]-dizi[j]);
-            }
-        }
-        
-    }
-    printf("Farklari sifira en yakin iki eleman : %d\n",yakinlik);
-        
+    // for (int i = 0; i < uzunluk; i++)
+    // {
+    //     printf("%d. eleman : ",i+1);
+    //     scanf("%d",&dizi[i]);
+    // }
+    // int yakinlik=abs(dizi[0]-dizi[1]);
+    // for (int i = 0; i < uzunluk; i++)
+    // {
+    //     for (int j = 0; j < uzunluk; j++)
+    //     {
+    //         if(i!=j && yakinlik>abs(dizi[i]-dizi[j])){
+    //             yakinlik = abs(dizi[i]-dizi[j]);
+    //         }
+    //     }
 
-    int eleman;
-    printf("Kacinci elemani silmek istersiniz : ");
-    scanf("%d",&eleman);
-    dizi[eleman-1]='\0';
-    printf("Yeni dizi : ");
+    // }
+    // printf("Farklari sifira en yakin iki eleman : %d\n",yakinlik);
+
+    // int eleman;
+    // printf("Kacinci elemani silmek istersiniz : ");
+    // scanf("%d",&eleman);
+    // dizi[eleman-1]='\0';
+    // printf("Yeni dizi : ");
+    // for (int i = 0; i < uzunluk; i++)
+    // {
+    //     if(dizi[i]!='\0'){
+    //         printf("%d ",dizi[i]);
+    //     }
+    // }
+    // for (int i = 0; i < uzunluk; i++)
+    // {
+    //     for (int j = 0; j < uzunluk-i; j++)
+    //     {
+    //         if(dizi[j]>dizi[j+1]){
+    //             int temp=dizi[j];
+    //             dizi[j]=dizi[j+1];
+    //             dizi[j+1]=temp;
+    //         }
+    //     }
+
+    // }
+    // printf("\nikinci en buyuk eleman : %d",dizi[uzunluk-2]);
+
     for (int i = 0; i < uzunluk; i++)
     {
-        if(dizi[i]!='\0'){
-            printf("%d ",dizi[i]);
-        }
+        int random = ceil(rand() / (float)RAND_MAX * 10);
+        dizi[i] = random;
     }
+    printf("Dizi : ");
     for (int i = 0; i < uzunluk; i++)
     {
-        for (int j = 0; j < uzunluk-i; j++)
-        {
-            if(dizi[j]>dizi[j+1]){
-                int temp=dizi[j];
-                dizi[j]=dizi[j+1];
-                dizi[j+1]=temp;
-            }
-        }
-        
+        printf("%d ", dizi[i]);
     }
-    printf("\nikinci en buyuk eleman : %d",dizi[uzunluk-2]);
-
-    
-
+    int mod_sayisi;
+    printf("\nButun dizinin kac ile modunun alinmasini istersiniz : ");
+    scanf("%d", &mod_sayisi);
+    for (int i = 0; i < uzunluk; i++)
+    {
+        dizi[i] = dizi[i] % mod_sayisi;
+    }
+    printf("\nModlu dizi : ");
+    for (int i = 0; i < uzunluk; i++)
+    {
+        printf("%d ", dizi[i]);
+    }
 }
