99f79efb690f000377650f81148632604fd8fa98
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-05-22 11:48:21 +0300

    Add files via upload

--- ders/ders15.c
+++ ders/ders15.c
@@ -2,13 +2,33 @@
 
 int main()
 {
-
-    for (int i = 10; i < 210; i++)
+    int num1;
+    int num2;
+    printf("num1 : ");
+    scanf("%d", &num1);
+    printf("num2 : ");
+    scanf("%d", &num2);
+    int buyuk;
+    int kucuk;
+    if (num1 > num2)
     {
-        while (i != 0)
-        {
-            i = i / 10;
-            printf("%d\n ", i % 10);
-        }
+        buyuk = num1;
+        kucuk = num2;
     }
+    else
+    {
+        buyuk = num2;
+        kucuk = num1;
+    }
+    static int toplam1 = 1;
+    static int toplam2 = 1;
+    for (int i = 1; i < buyuk; i++)
+    {
+        toplam1 *= i;
+    }
+    for (int i = 1; i < kucuk; i++)
+    {
+        toplam2 *= i;
+    }
+    printf("per(%d,%d): %f", buyuk, kucuk, (float)toplam1 / toplam2);
 }
