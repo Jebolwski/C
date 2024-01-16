83b298e5772a73e3a4e19c5071f2fb215cfc2c15
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-01-16 20:15:20 +0300

    Add files via upload

--- talo/fonks.c
+++ talo/fonks.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 
-char* degis(char charDizi[],char degiscek,char yenisi){
-    for (int i = 0; i < strlen(charDizi); i++)
-    {
-        if (charDizi[i] == degiscek){
-            charDizi[i] = yenisi;
-        }
+int sonuc(int parametre,int dizi[],int size){
+	int n=size;
+	float toplam=0.0,varyans=0.0,ss=0.0;
+	float ortalama=0.0;
+	
+	
+	for (int i = 0; i < n; i++) {
+        toplam += dizi[i];
     }
-
-    return charDizi;
+    ortalama = toplam / n;
+    
+	for (int i = 0; i < n; i++) {
+		varyans += pow(dizi[i] - ortalama, 2);
+    }
+    varyans /= n;
+    
+	if(parametre==1){//ss
+	return varyans;
+	
+	}
+	if(parametre==0){//varyans
+	ss=pow(varyans,0.5);
+	return ss;	
+	}
+	
 }
 
-
-
 int main(){
-    char string[50]="MERHABA";
-    char neyi;
-    printf("Neyi : ");
-    scanf("%c",&neyi);
-    char neyle;
-    printf("Neyi : ");
-    scanf("%c",&neyle);
-    printf("Onceki hali : %s\n",string);
-    printf("Son hali : %s",degis(string,'B','X'));
-    
+	int istek;
+	int dizii[5]={1,2,3,4,5};
+	printf("ne istiyorsun 1=varyans 0=ss : ");
+	scanf("%d",&istek);
+	printf("%d",sonuc(istek,dizii,5));
 }
